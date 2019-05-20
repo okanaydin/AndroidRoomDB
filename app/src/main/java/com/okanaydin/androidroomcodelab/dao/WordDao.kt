72501400c86066074126f985202a773294615e8c
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-05-20 05:24:46 +0300

    Update WordDao.kt

--- app/src/main/java/com/okanaydin/androidroomcodelab/dao/WordDao.kt
+++ app/src/main/java/com/okanaydin/androidroomcodelab/dao/WordDao.kt
@@ -22,10 +22,8 @@ interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
     fun getAllWords(): LiveData<List<Word>>
 
-    @Insert
-    fun insert(word: Word)
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
     fun deleteAll()
